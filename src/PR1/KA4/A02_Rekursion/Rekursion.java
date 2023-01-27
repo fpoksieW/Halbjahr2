42501ff999d15d101c7b2ea40343ee4f1c9ccc72
fpoksiew <2022-i13-weiskopf@s.gds2.de>
2023-01-27 09:17:54 +0100

    Motorrad aufgabe

---
+++ src/PR1/KA4/A02_Rekursion/Rekursion.java
@@ -0,0 +1,26 @@
+package PR1.KA4.A02_Rekursion;
+
+import java.util.Scanner;
+
+public class Rekursion {
+
+    public static void lieszeichen() {
+        Scanner sc = new Scanner(System.in);
+        char z;
+        z= sc.nextLine().charAt(0);
+        if (z != '_') {
+            lieszeichen();
+        }
+        System.out.println(z);
+        sc.close();
+
+    }
+    public static void main(String[] args) {
+        //Methode lieszeichen
+        System.out.println("Geben sie eine Zeile ein, solange die Zeile nicht mit _ anfängt, wird sie wiederholt ausgegeben.");
+        lieszeichen();
+        System.out.println("Ende der Rekursion");
+
+
+    }
+}
