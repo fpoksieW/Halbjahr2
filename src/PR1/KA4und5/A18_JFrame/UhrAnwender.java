86b060e4b23aa738e0181f8716dcf9286d3a9074
fpoksiew <2022-i13-weiskopf@s.gds2.de>
2023-04-20 11:55:46 +0200

    JFrame

--- src/PR1/KA4und5/A18_JFrame/UhrAnwender.java
+++ src/PR1/KA4und5/A18_JFrame/UhrAnwender.java
@@ -1,9 +1,27 @@
 package PR1.KA4und5.A18_JFrame;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.util.Date;
 
 public class UhrAnwender {
     public static void main(String[] args) {
         JFrame andiFrame = new JFrame("Uhrzeit in der ADV");
+
+        andiFrame.setSize(400, 300);
+        andiFrame.setBounds(200, 200, 400, 400);
+        andiFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel label = new JLabel("Uhrzeit ist:");
+        JLabel label1 = new JLabel(String.format("%tT", new Date()));
+        andiFrame.add(label);
+        andiFrame.add(label1);
+        label.setBounds(10, 10, 100, 20);
+
+        andiFrame.setVisible(true);
+        while(true) {
+            label1.setText(String.format("%tT", new Date()));
+            label1.setSize(100, 20);
+            andiFrame.setTitle(String.format("%tT", new Date()));
+        }
     }
 }
