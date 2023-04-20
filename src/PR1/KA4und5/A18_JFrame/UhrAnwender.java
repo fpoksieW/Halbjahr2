0502bcf696e829140a7e8d2a116d4a949157cbd9
fpoksiew <2022-i13-weiskopf@s.gds2.de>
2023-04-20 10:30:30 +0200

    JFrame

---
+++ src/PR1/KA4und5/A18_JFrame/UhrAnwender.java
@@ -0,0 +1,9 @@
+package PR1.KA4und5.A18_JFrame;
+
+import javax.swing.JFrame;
+
+public class UhrAnwender {
+    public static void main(String[] args) {
+        JFrame andiFrame = new JFrame("Uhrzeit in der ADV");
+    }
+}
